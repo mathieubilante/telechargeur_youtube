14d36097331d57353105efae1e5e37e2fa819e4a
mathieubilante <m97782840@gmail.com>
2025-10-26 00:30:16 +0000

    Modification du script telechargeur.py

--- telechargeur.py
+++ telechargeur.py
@@ -1,2 +1,18 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
+url = input("Enter le lien video youtube: ")
+print("Telechargement en cours...")
+print("\n")
+youtube_lien=YouTube(url, on_progress_callback=on_progress)
+#affichade des truc du lien 
+print("Titre: ", youtube_lien.title)
+print("Auteur: ", youtube_lien.author)
+print("Nombre de vues: ", youtube_lien.views)
+print("Durée: ", youtube_lien.length, "secondes")
+print("Description: ", youtube_lien.description)
+print("\n")
+stream = youtube_lien.streams.get_highest_resolution()
+stream.download()
+print("Telechargement terminé!")
+
+
